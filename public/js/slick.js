9c3d184455a8b48e08136a266003f2b1f2009eeb
ntbao10122003 <nguyenthebao1711@gmail.com>
2024-05-02 22:48:18 +0700

    test

---
+++ public/js/slick.js
@@ -0,0 +1,18 @@
+// <!-- slick -->
+$(document).ready(function () {
+ 
+  $(".home-slider").slick({
+    arrows: false,
+
+    speed: 300,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: false,
+    autoplay: true,
+    speed: 300,
+    autoplaySpeed: 3000,
+  
+  });
+})
+
+// <!-- end slick  -->
